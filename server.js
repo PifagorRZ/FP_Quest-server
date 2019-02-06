b1737786ada32732a47585cbcc3e71e9ccbcaefd
Pifagor <zheka.rachkovan@gmail.com>
2019-02-06 22:44:11 +0300

    seems good

--- server.js
+++ server.js
@@ -1,24 +1,105 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
 
 const app = express();
+let db;
 
-const urlencodedParser = bodyParser.urlencoded({extended: false});
+app.use(bodyParser.urlencoded({
+    extended: true
+  }))
 
-app.post('/result', urlencodedParser, (req, res) => {
-    if (!req.body) {
-        res.sendStatus(400);
+app.use(bodyParser.json())
+
+
+
+app.put('/addTimeInGame', (req, res) => {
+    console.log('add time in game')
+    db.collection('Users').updateOne({}, {$inc: {"timeInGame": req.body.amount}})
+    .then(() => {
+        res.sendStatus(200)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+})
+
+app.put('/addSymbols', (req, res) => {
+    console.log("add symbols")
+    db.collection('Users').updateOne({}, {$inc: {"symbols": req.body.amount}})
+    .then(() => {
+        res.sendStatus(200)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+})
+
+app.put('/addGoodRuns', (req, res) => {
+    console.log('add time in game')
+    db.collection('Users').updateOne({}, {$inc: {"goodRuns": 1}})
+    .then(() => {
+        res.sendStatus(200)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+})
+
+app.put('/addBadRuns', (req, res) => {
+    console.log('add time in game')
+    db.collection('Users').updateOne({}, {$inc: {"badRuns": 1}})
+    .then(() => {
+        res.sendStatus(200)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+})
+
+app.put('/sign-up', (req, res) => {
+    console.log('newUser')
+    db.collection('Users').updateOne({}, {$inc: {"users": 1}})
+    .then(() => {
+        res.sendStatus(200)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+})
+
+app.put('/log-in', (req, res) => {
+    console.log('autorized')
+    db.collection('Users').updateOne({}, {$inc: {"activeUsers": 1}})
+    .then(() => {
+        res.sendStatus(200)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+})
+
+app.put('/log-out', (req, res) => {
+    console.log('un-autorized')
+    db.collection('Users').updateOne({}, {$inc: {"activeUsers": -1}})
+    .then(() => {
+        res.sendStatus(200)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+})
+
+MongoClient.connect('mongodb+srv://freddy2010:qweQWE333@list-0ixqy.mongodb.net/test?retryWrites=true', (err, database) => {
+    if (err) {
+        return console.log(err)
     }
-    res.send(`Ваш заказ принят
-    Заказано изделие: ${req.body.type}
-    Материал ${req.body.material}
-    Количество ${req.body.amount}`)
-
+    db = database.db('FP_Quest');
+    app.listen(3012, () => {
+        console.log("Server started");
+    });
 })
 
-app.get('/', urlencodedParser, (req, res) => {
-    res.sendFile(__dirname + "/index.html");
-})
 
-app.listen(3000);
 
